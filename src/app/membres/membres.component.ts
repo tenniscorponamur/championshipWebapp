59211d19c93e9fedacceb90145ccaabb6f345e95
fabricecalay <fabrice.calay@gmail.com>
2018-03-16 18:58:06 +0100

    test ngclass --> non-concluant (ecran mouvant, je ne trouve pas ca ergonomique)

--- src/app/membres/membres.component.ts
+++ src/app/membres/membres.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-membres',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MembresComponent implements OnInit {
 
+  memberListClass:string = "tennisCorpoBox col-sm-12 col-md-12 col-lg-6 col-xl-6";
+  selectedMember:string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openMember():void{
+    this.selectedMember="test";
   }
 
 }
