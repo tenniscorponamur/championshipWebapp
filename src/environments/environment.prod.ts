9eebfa0dc56ae533d9020515301ed32c7a63fa9c
fabricecalay <fabrice.calay@gmail.com>
2018-04-02 16:10:42 +0200

    preparation prototype (administration - referentiels)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   clientId: 'tenniscorpoclientid',
   clientPassword: 'ABi8u34kPoDo',
   tokenUrl: 'http://localhost:9100/oauth/token', // URL to auth api
-  publicApiUrl: 'http://localhost:9100/api/public',  // URL to public web api
-  privateApiUrl: 'http://localhost:9100/api/private'  // URL to private web api
+  publicApiUrl: 'http://localhost:9100/api/v1/public',  // URL to public web api
+  privateApiUrl: 'http://localhost:9100/api/v1/private'  // URL to private web api
 };
